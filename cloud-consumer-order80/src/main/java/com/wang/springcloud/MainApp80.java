4f4d8f4b0d415d7905660d170a49aa428e5c3ffc
haimianbabe <1546413670@qq.com>
2023-03-23 10:20:50 +0800

    2023.03.23

---
+++ cloud-consumer-order80/src/main/java/com/wang/springcloud/MainApp80.java
@@ -0,0 +1,12 @@
+package com.wang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApp80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApp80.class,args);
+    }
+}
---
+++ cloud-consumer-order80/src/main/java/com/wang/springcloud/controller/OrderController.java
@@ -0,0 +1,28 @@
+package com.wang.springcloud.controller;
+
+import com.wang.springcloud.entities.CommonResult;
+import com.wang.springcloud.entities.Payment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class OrderController {
+
+    private static final String PaymentSvl_url = "http://localhost:8001";
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/create")
+    public CommonResult create(Payment payment){
+        return restTemplate.postForObject(PaymentSvl_url+"/payment/create",payment,CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/get/{id}")
+    public CommonResult getPayment(@PathVariable Long id){
+        return restTemplate.getForObject(PaymentSvl_url+"/payment/get/"+id,CommonResult.class,id);
+    }
+}
