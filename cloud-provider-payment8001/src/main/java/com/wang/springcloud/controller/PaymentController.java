b83a0fc101b24c24b3133dbdd547590c124aa956
haimianbabe <1546413670@qq.com>
2023-03-23 17:38:52 +0800

    2023.03.23 eureka更多配置、consul生产者创建

--- cloud-provider-payment8001/src/main/java/com/wang/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/wang/springcloud/controller/PaymentController.java
@@ -6,7 +6,11 @@ import com.wang.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -17,6 +21,9 @@ public class PaymentController {
 
     @Autowired
     PaymentService paymentService;
+
+    @Autowired
+    DiscoveryClient discoveryClient;
 
     @PostMapping("/payment/create")
     public CommonResult create(@RequestBody Payment payment){
@@ -33,8 +40,7 @@ public class PaymentController {
 
 
     @GetMapping(value = "/payment/get/{id}")
-    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id)
-    {
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id) {
         Payment payment = paymentService.getPaymentById(id);
         log.info("*****查询结果:{}",payment);
         if (payment != null) {
@@ -42,6 +48,20 @@ public class PaymentController {
         }else{
             return new CommonResult(444,"没有对应记录,查询ID: "+id,null);
         }
+    }
+
+    @GetMapping("/payment/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        for(String element : services){
+            System.out.println(element);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance element : instances){
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+                    + element.getUri());
+        }
+        return this.discoveryClient;
     }
 
 }
